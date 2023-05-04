29f81108a39580b4e80a0bedb25ae18d2fc919ba
root <mgiraudo@ies21.edu.ar>
2023-05-04 13:33:35 -0300

    Agregar archivos de proyecto.

---
+++ pryfermoya/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryfermoya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection conn = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=biblio.accdb;");
+            conn.Open();
+
+            MessageBox.Show("se conectooooo");
+
+        }
+    }
+}
