308aa9e05f03b34ff299afbc238b40834d388b2b
root <mgiraudo@ies21.edu.ar>
2023-05-04 14:03:53 -0300

    cambios base de datos

--- pryfermoya/Form1.Designer.cs
+++ pryfermoya/Form1.Designer.cs
@@ -28,34 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.btnLeerTabla = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnConexion
             // 
-            this.button1.Location = new System.Drawing.Point(274, 93);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(230, 102);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "conexion a base de datos";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnConexion.Location = new System.Drawing.Point(57, 51);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(150, 43);
+            this.btnConexion.TabIndex = 0;
+            this.btnConexion.Text = "conexion a base de datos";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConeccion_Click);
+            // 
+            // btnLeerTabla
+            // 
+            this.btnLeerTabla.Location = new System.Drawing.Point(57, 308);
+            this.btnLeerTabla.Name = "btnLeerTabla";
+            this.btnLeerTabla.Size = new System.Drawing.Size(146, 41);
+            this.btnLeerTabla.TabIndex = 1;
+            this.btnLeerTabla.Text = "Leer tabla libros";
+            this.btnLeerTabla.UseVisualStyleBackColor = true;
+            this.btnLeerTabla.Click += new System.EventHandler(this.btnLeerTabla_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgvDatos.Location = new System.Drawing.Point(57, 122);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(240, 150);
+            this.dgvDatos.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(358, 415);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnLeerTabla);
+            this.Controls.Add(this.btnConexion);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "base de datos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnConexion;
+        private System.Windows.Forms.Button btnLeerTabla;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
     }
 }
 
--- pryfermoya/Form1.cs
+++ pryfermoya/Form1.cs
@@ -17,14 +17,36 @@ namespace pryfermoya
         {
             InitializeComponent();
         }
-
+        // declaracion de variables para este formulario
         OleDbConnection conn = null;
-        private void button1_Click(object sender, EventArgs e)
+        OleDbCommand cmd = null;
+        OleDbDataReader LectorBD = null;
+
+        private void btnConeccion_Click(object sender, EventArgs e)
         {
             conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=biblio.accdb;");
             conn.Open();
 
             MessageBox.Show("se conectooooo");
+
+        }
+
+        private void btnLeerTabla_Click(object sender, EventArgs e)
+        {
+            cmd = new OleDbCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "TITULO";
+
+            MessageBox.Show("Conexion a " + cmd.CommandText, "mensaje de conexion");
+            
+            LectorBD = cmd.ExecuteReader();
+
+            while (LectorBD.Read()) 
+            {
+                dgvDatos.Rows.Add(LectorBD[0]);
+            }
 
         }
     }
